803cc4000bf9d3f284b4ea4ffd54e3df478b1dc1
Cassy Myers <cassymyers@gmail.com>
2018-10-20 13:47:17 +0900

    change doc search fxn

--- lib/macbeth_analyzer.rb
+++ lib/macbeth_analyzer.rb
@@ -6,11 +6,13 @@ xml_file = open(url).read
 # file = File.open()
 document = Nokogiri::XML(xml_file)
 
-document.root.xpath('speech').each do |speech|
-  name        = speech.xpath('speaker').text
-  line  = speech.xpath('line')
-  origin      = speech.xpath('origin').text
 
+
+document.search('speech').each do |speech|
+  name = speech.xpath('speaker').text
+  p name
+  line = speech.xpath('line')
+  origin = speech.xpath('origin').text
   puts "#{name}, a #{line.count}"
 end
 
