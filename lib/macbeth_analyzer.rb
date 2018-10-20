56368a5edf0c732fe354201d74ff3be8ddeb4385
Cassy Myers <cassymyers@gmail.com>
2018-10-20 14:37:57 +0900

    refactor and add comments

--- lib/macbeth_analyzer.rb
+++ lib/macbeth_analyzer.rb
@@ -3,23 +3,17 @@ require 'open-uri'
 
 url = "http://www.ibiblio.org/xml/examples/shakespeare/macbeth.xml"
 xml_file = open(url).read
-# file = File.open()
-doc = Nokogiri::XML(xml_file)
+doc = Nokogiri::XML(xml_file) #grab file from internet
 
-lines = Hash.new
+lines = Hash.new #create a hash to store the speaker names and the # of lines
 
 doc.search('//SPEECH').each do |speech|
   name = speech.at('SPEAKER').text
-  lines[name] = 0 if lines[name].nil?
-  line_count = speech.xpath('LINE').count
-  lines[name] += line_count
-  # name = speech.xpath('speaker').text.strip
-  # p name
-  # line = speech.xpath('line')
-  # origin = speech.xpath('origin').text
-  # puts "#{name}, a #{line.count}"
+  lines[name] = 0 if lines[name].nil? #create the key and set it to 0, won't override otherwise
+  line_count = speech.xpath('LINE').count #grab the number of lines per speech of character
+  lines[name] += line_count #add the number back to the hash
 end
-lines.delete('ALL')
+lines.delete('ALL') # delete the "all" value from the hash, don't need it.
 lines.each do |speaker|
-  puts "#{speaker[1]} #{speaker[0]}"
+  puts "#{speaker[1]} #{speaker[0]}" #push to console.
 end
