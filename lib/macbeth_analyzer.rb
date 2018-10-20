c5b841f3c7f40e16b6935feb2d84902dfab54019
Cassy Myers <cassymyers@gmail.com>
2018-10-20 14:35:39 +0900

    ruby macbeth analyser works in correct format for the speech

--- lib/macbeth_analyzer.rb
+++ lib/macbeth_analyzer.rb
@@ -6,16 +6,20 @@ xml_file = open(url).read
 # file = File.open()
 doc = Nokogiri::XML(xml_file)
 
-
+lines = Hash.new
 
 doc.search('//SPEECH').each do |speech|
   name = speech.at('SPEAKER').text
-  puts "name = #{name}"
+  lines[name] = 0 if lines[name].nil?
+  line_count = speech.xpath('LINE').count
+  lines[name] += line_count
   # name = speech.xpath('speaker').text.strip
   # p name
   # line = speech.xpath('line')
   # origin = speech.xpath('origin').text
   # puts "#{name}, a #{line.count}"
 end
-
-
+lines.delete('ALL')
+lines.each do |speaker|
+  puts "#{speaker[1]} #{speaker[0]}"
+end
